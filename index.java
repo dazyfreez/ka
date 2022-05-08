ee0b5da0d680918f6a0a4b2b5f53f8aa0bdd9946
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-08 13:59:30 +0200

    -

--- index.java
+++ index.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 public class index {
     public static void main(String[] args) {
-        
+        System.out.println("Wie lange soll das array sein?");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        sc.close();
+        System.out.println("Bitte geben Sie die Werte ein:");
+        Scanner sc2 = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            array[i] = sc2.nextInt();
+            System.out.println("Der Wert " + i + " ist " + array[i]);
+        }
+        sc2.close();
     }
 }
