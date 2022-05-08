2cdf9836f1b2398ef1a120b775f104494f04696c
dazyfreez <74270036+dazyfreez@users.noreply.github.com>
2022-05-08 14:00:39 +0200

    -

--- index.java
+++ index.java
@@ -6,12 +6,6 @@ public class index {
         int n = sc.nextInt();
         int[] array = new int[n];
         sc.close();
-        System.out.println("Bitte geben Sie die Werte ein:");
-        Scanner sc2 = new Scanner(System.in);
-        for (int i = 0; i < n; i++) {
-            array[i] = sc2.nextInt();
-            System.out.println("Der Wert " + i + " ist " + array[i]);
-        }
-        sc2.close();
+        
     }
 }
